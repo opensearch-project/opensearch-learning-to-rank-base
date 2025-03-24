c2b461df3ee3e350aa5c0c5a999daf58b5286676
Platon Bibik <platon.bibik@autoscout24.com>
2025-03-24 11:09:39 +0100

    add an ensemble test
    Signed-off-by: Platon Bibik <platon.bibik@autoscout24.com>

--- src/test/java/com/o19s/es/ltr/ranker/parser/XGBoostRawJsonParserTests.java
+++ src/test/java/com/o19s/es/ltr/ranker/parser/XGBoostRawJsonParserTests.java
@@ -579,4 +579,91 @@ public class XGBoostRawJsonParserTests extends LuceneTestCase {
             CoreMatchers.containsString("Unknown features in model: [feat1, feat2]")
         );
     }
+
+    public void testMultiTreeEnsemble() throws IOException {
+        String model = "{"
+                + "    \"learner\":{"
+                + "        \"attributes\":{},"
+                + "        \"feature_names\":[\"feat1\", \"feat2\"],"
+                + "        \"feature_types\":[\"float\", \"float\"],"
+                + "        \"gradient_booster\":{"
+                + "        \"model\":{"
+                + "            \"gbtree_model_param\":{"
+                + "            \"num_parallel_tree\":\"1\","
+                + "            \"num_trees\":\"2\"},"
+                + "            \"iteration_indptr\":[0,1,2],"
+                + "            \"tree_info\":[0, 1],"
+                + "            \"trees\":[{"
+                + "                \"base_weights\":[1E0, 10E0, 0E0],"
+                + "                \"categories\":[],"
+                + "                \"categories_nodes\":[],"
+                + "                \"categories_segments\":[],"
+                + "                \"categories_sizes\":[],"
+                + "                \"default_left\":[0, 0, 0],"
+                + "                \"id\":0,"
+                + "                \"left_children\":[2, -1, -1],"
+                + "                \"loss_changes\":[0E0, 0E0, 0E0],"
+                + "                \"parents\":[2147483647, 0, 0],"
+                + "                \"right_children\":[1, -1, -1],"
+                + "                \"split_conditions\":[3E0, -1E0, -1E0],"
+                + "                \"split_indices\":[0, 0, 0],"
+                + "                \"split_type\":[0, 0, 0],"
+                + "                \"sum_hessian\":[1E0, 1E0, 1E0],"
+                + "                \"tree_param\":{"
+                + "                    \"num_deleted\":\"0\","
+                + "                    \"num_feature\":\"2\","
+                + "                    \"num_nodes\":\"3\","
+                + "                    \"size_leaf_vector\":\"1\"}"
+                + "                },"
+                + "                {"
+                + "                \"base_weights\":[1E0, 5E0, 0E0],"
+                + "                \"categories\":[],"
+                + "                \"categories_nodes\":[],"
+                + "                \"categories_segments\":[],"
+                + "                \"categories_sizes\":[],"
+                + "                \"default_left\":[0, 0, 0],"
+                + "                \"id\":1,"
+                + "                \"left_children\":[2, -1, -1],"
+                + "                \"loss_changes\":[0E0, 0E0, 0E0],"
+                + "                \"parents\":[2147483647, 0, 0],"
+                + "                \"right_children\":[1, -1, -1],"
+                + "                \"split_conditions\":[3E0, -1E0, -1E0],"
+                + "                \"split_indices\":[1, 1, 1],"
+                + "                \"split_type\":[0, 0, 0],"
+                + "                \"sum_hessian\":[1E0, 1E0, 1E0],"
+                + "                \"tree_param\":{"
+                + "                    \"num_deleted\":\"0\","
+                + "                    \"num_feature\":\"2\","
+                + "                    \"num_nodes\":\"3\","
+                + "                    \"size_leaf_vector\":\"1\"}"
+                + "                }"
+                + "            ]},"
+                + "            \"name\":\"gbtree\""
+                + "        },"
+                + "        \"learner_model_param\":{"
+                + "            \"base_score\":\"5E-1\","
+                + "            \"boost_from_average\":\"1\","
+                + "            \"num_class\":\"0\","
+                + "            \"num_feature\":\"2\","
+                + "            \"num_target\":\"1\""
+                + "        },"
+                + "        \"objective\":{"
+                + "            \"name\":\"reg:linear\","
+                + "            \"reg_loss_param\":{\"scale_pos_weight\":\"1\"}"
+                + "        }"
+                + "    },"
+                + "    \"version\":[2,1,0]"
+                + "}";
+
+        FeatureSet set = new StoredFeatureSet("set", List.of(randomFeature("feat1"), randomFeature("feat2")));
+        NaiveAdditiveDecisionTree tree = parser.parse(set, model);
+        FeatureVector featureVector = tree.newFeatureVector(null);
+        featureVector.setFeatureScore(0, 2);
+        featureVector.setFeatureScore(1, 2);
+        assertEquals(0.0, tree.score(featureVector), Math.ulp(0.1F));
+
+        featureVector.setFeatureScore(0, 4);
+        featureVector.setFeatureScore(1, 4);
+        assertEquals(15.0, tree.score(featureVector), Math.ulp(0.1F));
+    }
 }
